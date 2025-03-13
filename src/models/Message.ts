9e6be4613abf88748b8e098c5fe9aaef119812dc
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-03-13 15:29:33 +0700

    Create Message.ts

---
+++ src/models/Message.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+const MessageSchema = new mongoose.Schema(
+  {
+    role: { type: String, enum: ["user", "assistant"], required: true },
+    content: { type: String, required: true },
+    thought: { type: String },
+    thread_id: { type: String, ref: "Thread", required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Message || mongoose.model("Message", MessageSchema);
+
