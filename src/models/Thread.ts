60072a97f1e0196dde7aeec118a3b94495e520ec
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-03-13 15:29:35 +0700

    Create Thread.ts

---
+++ src/models/Thread.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+
+const ThreadSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true, unique: true },
+    title: { type: String, required: true },
+    user_id: { type: String, ref: "User", required: true }, 
+  },
+  { timestamps: true }
+);
+
+// ThreadSchema.virtual("id_thread").get(function () {
+//   return this._id;
+// });
+export default mongoose.models.Conversation || mongoose.model("Thread", ThreadSchema);
