947f88f4090ee1ba85f35551d4d1030492d24192
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-03-30 09:35:39 +0700

    fixing button bugs and helper bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,23 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div 
+          id="loading" 
+          style={{
+            display: 'none',
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'rgba(0,0,0,0.5)',
+            zIndex: 9999,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <div>Loading...</div>
+        </div>
         {children}
       </body>
     </html>
