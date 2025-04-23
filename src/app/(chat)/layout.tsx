5c89780817ae2161940526220fb8bed390637100
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-04-23 21:02:42 +0700

    update chat feature

--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -1,6 +1,8 @@
 "use client"; // Tambahkan ini di paling atas
 
 import { ChatSidebar } from "@/components/ChatSidebar";
+import HistoryChat from "@/components/HistoryChat";
+import { ThemeProvider } from "@/components/ThemeProvider";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { useState } from "react";
 
@@ -12,11 +14,15 @@ export default function ChatLayout({
   const [sidebarOpen, setSidebarOpen] = useState(true);
 
   return (
-    <SidebarProvider open={sidebarOpen} onOpenChange={setSidebarOpen}>
-      <div className="flex h-screen bg-background  w-full">
-        <ChatSidebar />
-        {children} {/* Gunakan children bukan impor langsung */}
-      </div>
-    </SidebarProvider>
+    
+      
+    
+      <SidebarProvider open={sidebarOpen} onOpenChange={setSidebarOpen}>
+        <div className="flex h-screen bg-background justify-between w-full">
+          <ChatSidebar />
+          {children} {/* Gunakan children bukan impor langsung */}
+        </div>
+      </SidebarProvider>
+    
   );
 }
