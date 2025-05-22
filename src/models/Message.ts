e5a0d0d42ccad722c43785bd480c366d90fad5b8
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2025-05-22 20:16:28 +0700

    Update Message.ts

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -5,6 +5,7 @@ const MessageSchema = new mongoose.Schema(
     role: { type: String, enum: ["user", "assistant"], required: true },
     content: { type: String, required: true },
     thought: { type: String },
+    correction: { type: String },
     thread_id: { type: String, ref: "Thread", required: true },
   },
   { timestamps: true }
